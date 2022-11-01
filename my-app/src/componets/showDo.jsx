1c731aac02b8f50564fdda9a475f9876419bc23c
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-01 14:55:40 -0700

    small changes

---
+++ my-app/src/componets/showDo.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+import todoForm from "./Todo";
+
+const showCode = (e) =>{
+    todoForm();
+    return(
+        <div>
+            Hello
+        </div>
+    );
+}
