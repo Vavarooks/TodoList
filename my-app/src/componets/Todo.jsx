86ded17cfe4910d3651940c001cc1c32bc558ec9
Vanessa Emerick <vavarooks123@gmail.com>
2022-11-01 15:32:29 -0700

    Small changes to To do lists

--- my-app/src/componets/Todo.jsx
+++ my-app/src/componets/Todo.jsx
@@ -1,49 +1,53 @@
 import React, {useState} from "react";
 
-const coding = [
-    'Python',
-    'HTML',
-    'Javascript',
-    'Java',
-    'CSS',
-    'C#'
-];
-export default function todoForm(){
-    const [selectedChoice, setSelectedChoice] = useState(coding[0]);
-    const [isPicked, setIsPicked] = useState(false);
-    
-    function handleSubmit(event){
-        event.preventDefault();
-        console.log("Form Submitted");
-        
-    }
+const newCode = (e) =>{
 
-    return(
-        <>
-            <ul>
-                {coding.map((choice, i) =>
-                    <li key={i}>{choice}</li>
-                )}
-            </ul>
-            <div class="card d-block mx-auto w-50 my-3 border p-3">
-                <h1>To do List</h1>
+    const coding = [
+        'Python',
+        'HTML',
+        'Javascript',
+        'Java',
+        'CSS',
+        'C#'
+    ];
+    
+        return(
+            <>
                 <ul>
-                    <ls>{choice}</ls>
+                    {coding.map((choice, i) =>
+                        <li key={i}>{choice}</li>
+                    )}
                 </ul>
-            </div>
-            <div class="card d-block mx-auto w-50 my-3 border p-3">
-                <form onSubmit={handleSubmit}>
-                    <select value={selectedChoice} onChange={e => setSelectedChoice}>
-                        {coding.map((choice, idx) => 
-                            <option key={idx} value={choice}>{choice}</option>
-                        )}
-                    </select>
-                    <label class="form-lable">Add {choice}?</label>
-                        <input class="form-control" type="checkbox" checked={isPicked} onChange={ e=> 
-                            setIsPicked(e.target.value)}/>
-                    <button class="btn btn-secondary">Add To List</button>
-                </form>
-            </div>
-        </>
-    );
+                <div class="card d-block mx-auto w-50 my-3 border p-3">
+                    <h1>To do List</h1>
+                    <ul>
+                        <ls>{choice}</ls>
+                    </ul>
+                </div>
+                <div class="card d-block mx-auto w-50 my-3 border p-3">
+                    <form onSubmit={handleSubmit}>
+                        <select value={selectedChoice} onChange={e => setSelectedChoice}>
+                            {coding.map((choice, idx) => 
+                                <option key={idx} value={choice}>{choice}</option>
+                            )}
+                        </select>
+                        <label class="form-lable">Add {choice}?</label>
+                            <input class="form-control" type="checkbox" checked={isPicked} onChange={ e=> 
+                                setIsPicked(e.target.value)}/>
+                        <button class="btn btn-secondary">Add To List</button>
+                    </form>
+                </div>
+            </>
+        );
 }
+
+    export default function todoForm(){
+        const [selectedChoice, setSelectedChoice] = useState(coding[0]);
+        const [isPicked, setIsPicked] = useState(false);
+        
+        function handleSubmit(event){
+            event.preventDefault();
+            console.log("Form Submitted");
+            
+        }
+    } 
